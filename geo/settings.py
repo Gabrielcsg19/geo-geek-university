57c6f169418ca010e6b6bd80d420ca79a4942a8f
Gabriel Souza <54643425+Gabrielcsg19@users.noreply.github.com>
2021-03-13 19:49:09 -0300

    Update settings.py

--- geo/settings.py
+++ geo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8&+j#jv%_2p)t73rf_4@_6vid2q(=!9gme47$)5uu6#=1cu5we'
+SECRET_KEY = 'SECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
